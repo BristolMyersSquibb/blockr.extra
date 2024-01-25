6706e519704b2d62c6f5f1d240eb17abf60c280d
Christoph Sax <christoph.sax@gmail.com>
2024-01-25 12:40:20 +0100

    out factor ui_fields_one_column
    fix

--- R/filter-expr-block.R
+++ R/filter-expr-block.R
@@ -46,40 +46,7 @@ filter_expr_block <- function(data, ...) {
   initialize_block(new_filter_expr_block(data, ...), data)
 }
 
-
-# A simple, one column layout
-#
-# we need the fields, not the rendered fields, that's why I can't use the layout
-# methods
-
-#' @importFrom shiny p tags div
+#' @export
 ui_fields.filter_expr_block <- function(x, ns, inputs_hidden, ...) {
-
-  html_card <- function(field, field_name) {
-    div(
-      div(
-        tags$h5(attr(field, "title")),
-        div(
-          class = c("text-muted small mb-2"),
-          attr(field, "descr")
-        )
-      ),
-      tags$div(
-        class = "w-100",
-        ui_input(field, id = ns(field_name), name = NULL)
-      )
-    )
-  }
-
-  cards <- Map(
-    html_card,
-    field = x,
-    field_name = names(x)
-  )
-
-  div(
-    class = sprintf("block-inputs %s", inputs_hidden),
-    div(cards, class = "mt-3")
-  )
-
+  ui_fields_one_column(x = x, ns = ns, inputs_hidden = inputs_hidden)
 }
