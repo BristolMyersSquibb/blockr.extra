1531eef2debe2ee40fa42aa6e02a3b8b6c0770f8
Christoph Sax <christoph.sax@gmail.com>
2024-01-25 11:07:06 +0100

    Tests for Admiral Block

---
+++ tests/testthat/test-admiral-dpc-block.R
@@ -0,0 +1,21 @@
+test_that("admiral-dpc-block", {
+  data <- datasets::BOD
+  data$PARAMCD <- as.character(seq_len(nrow(data)))
+
+  block <- admiral_dpc_block(
+    data,
+    by_vars = "Time",
+    parameters = c("4", "5"),
+    set_values_to = c(
+      demand = "demand * 2",
+      PARAMCD = "'17'"
+    )
+  )
+
+  expect_s3_class(block, "admiral_dpc_block")
+  expect_type(block, "list")
+
+  res <- evaluate_block(block, data)
+  expect_identical(nrow(res), 8L)
+
+})
