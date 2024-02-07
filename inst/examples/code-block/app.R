392bee3e91ca01bcad7468b984d01511b3e078a4
John Coene <jcoenep@gmail.com>
2024-02-07 22:48:14 +0100

    feat: init code block

---
+++ inst/examples/code-block/app.R
@@ -0,0 +1,18 @@
+devtools::load_all()
+library(shiny)
+
+stack <- new_stack(
+  data_block,
+  code_transform_block
+)
+
+ui <- fluidPage(
+  theme = bslib::bs_theme(5L),
+  generate_ui(stack)
+)
+
+server <- function(input, output, session) {
+  generate_server(stack)
+}
+
+shinyApp(ui, server)
