d5e76cfd3b55994fdb555bb4da5841dee6d70e7f
John Coene <jcoenep@gmail.com>
2024-02-16 13:37:27 +0100

    chore: remove demo blocks

--- R/utils.R
+++ R/utils.R
@@ -35,23 +35,11 @@ ui_fields_one_column <- function(x, ns, inputs_hidden) {
 
 register_blockr_extra_blocks <- function(pkg) {
 
-  if (missing(pkg)) {
-    pkg <- pkg_name()
-  }
+  if (missing(pkg))
+    pkg <- "blockr.extra"
 
   register_blocks(
     constructor = c(
-      plot_block,
-      ggiraph_block,
-      asfactor_block,
-      lab_data_block,
-      demo_data_block,
-      demo_join_block,
-      demo_arrange_block,
-      demo_group_by_block,
-      demo_filter_block_1,
-      demo_filter_block_2,
-      demo_summarize_block,
       admiral_dpc_block,
       filter_expr_block,
       summarize_expr_block,
@@ -59,17 +47,6 @@ register_blockr_extra_blocks <- function(pkg) {
       code_plot_block
     ),
     name = c(
-      "plot block",
-      "ggiraph block",
-      "asfactor block",
-      "lab data_block",
-      "demo data block",
-      "demo join block",
-      "demo arrange block",
-      "demo group by block",
-      "demo filter block 1",
-      "demo filter block 2",
-      "demo summarize block",
       "admiral dpc block",
       "filter expr block",
       "summarize expr block",
@@ -77,17 +54,6 @@ register_blockr_extra_blocks <- function(pkg) {
       "code plot block"
     ),
     description = c(
-      "Monolithic ggplot block",
-      "Monolithic ggiraph block",
-      "As factor mutate block",
-      "blockr.data preselected to lab data",
-      "blockr.data preselected to demo data",
-      "Predefined join block for BMS demo",
-      "Predefined arrange block for BMS demo",
-      "Predefined group by block for BMS demo",
-      "Predefined filter block for BMS demo 1",
-      "Predefined filter block for BMS demo 2",
-      "Predefined summarize block for BMS demo",
       "Admiral block",
       "Filter expr block",
       "Summarize expr block",
@@ -95,17 +61,6 @@ register_blockr_extra_blocks <- function(pkg) {
       "Code plot block"
     ),
     classes = list(
-      c("plot_block", "submit_block"),
-      c("ggiraph_block", "plot_block", "submit_block"),
-      c("asfactor_block", "transform_block"),
-      c("lab_dataset_block", "data_block"),
-      c("demo_dataset_block", "data_block"),
-      c("demo_join_block", "transform_block", "submit_block"),
-      c("demo_arrange_block", "transform_block"),
-      c("demo_group_by_block", "transform_block"),
-      c("demo_filter_block_1", "transform_block", "submit_block"),
-      c("demo_filter_block_2", "transform_block", "submit_block"),
-      c("demo_summarize_block", "transform_block", "submit_block"),
       c("admiral_dpc_block", "transform_block"),
       c("filter_expr_block", "transform_block"),
       c("summarize_expr_block", "transform_block"),
@@ -113,35 +68,13 @@ register_blockr_extra_blocks <- function(pkg) {
       c("code_plot_block", "plot_block", "submit_block")
     ),
     input = c(
-      "data.frame",
-      "data.frame",
-      "data.frame",
       NA_character_,
-      NA_character_,
-      "data.frame",
-      "data.frame",
-      "data.frame",
-      "data.frame",
-      "data.frame",
-      "data.frame",
-      "data.frame",
       "data.frame",
       "data.frame",
       "data.frame",
       "data.frame"
     ),
     output = c(
-      "list",
-      "list",
-      "data.frame",
-      "data.frame",
-      "data.frame",
-      "data.frame",
-      "data.frame",
-      "data.frame",
-      "data.frame",
-      "data.frame",
-      "data.frame",
       "data.frame",
       "data.frame",
       "data.frame",
